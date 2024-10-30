faffc2b81f8e3b11173b56f74175c70336054a0b
Amelia <147034763+DivineVoices@users.noreply.github.com>
2024-10-30 19:09:11 +0100

    I LOVE SWITCHES
    I PUT MORE SWITCHES IN, I LOVE SWITCHES. BOY DO I LOVE SWITCHES DON'T YOU LOVE SWITCHES BECAUSE I SURE DO?

--- Genshin Bricks/Genshin Bricks.cpp
+++ Genshin Bricks/Genshin Bricks.cpp
@@ -11,6 +11,12 @@ int main()
         float x, y;
         Vector2(float _x = 0, float _y = 0) : x(_x), y(_y) {}
     };
+    sf::Font font;
+    // Load the font file; replace "path/to/font.ttf" with the actual path to your font file.
+    if (!font.loadFromFile("zh-cn.ttf")) {
+        std::cerr << "Error loading font\n";
+        return -1;
+    }
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
     Vector2 screenSize(600, 800);
@@ -31,7 +37,7 @@ int main()
     sf::RectangleShape Paddle(sf::Vector2f(paddleSize.x, paddleSize.y));
     sf::RectangleShape Brick(sf::Vector2f(brickSize.x, brickSize.y));
     sf::Vector2i mousePosition = sf::Mouse::getPosition();
-    Vector2 ballPos(300, 400);
+    Vector2 ballStartPos(300, 400);
     Vector2 speed(0.1f, 0.1f);
     sf::Time electroDuration;
     bool electroSpeed = false;
@@ -40,9 +46,11 @@ int main()
     Ball.setFillColor(Anemo);
     Paddle.setFillColor(Anemo);
     Brick.setFillColor(Electro);
-    Ball.setPosition(ballPos.x, ballPos.y);
+    Ball.setPosition(ballStartPos.x, ballStartPos.y);
     Paddle.setPosition(0, 750);
     window.setKeyRepeatEnabled(false);
+    sf::Text text;
+
 
     enum State
     {
@@ -50,6 +58,7 @@ int main()
         Reposition,
         Running,
         GameOver,
+        GameWin,
     };
 
     State gameState = Running;
@@ -58,6 +67,9 @@ int main()
 
     while (window.isOpen())
     {
+        sf::FloatRect paddleBox = Paddle.getGlobalBounds();
+        sf::FloatRect ballBox = Ball.getGlobalBounds();
+        sf::FloatRect brickBox = Brick.getGlobalBounds();
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -68,120 +80,179 @@ int main()
             {
                 if (event.mouseButton.button == sf::Mouse::Left)
                 {
-                    eleSwitcher += 1;
-                    if (eleSwitcher > 2) {
-                        eleSwitcher = 0;
+                    std::cout << "Left click registered" << std::endl;
+                    switch(gameState) {
+                    case Running: 
+                        std::cout << "Running left click registered" << std::endl;
+                        eleSwitcher += 1;
+                        if (eleSwitcher > 2) {
+                            eleSwitcher = 0;
+                        }
+                        Paddle.setFillColor(eleStorage[eleSwitcher]);
+                        break;
+
+                    case GameOver:
+                        std::cout << "GameOver left click registered" << std::endl;
+                        lives = 3;
+                        ballVisible = true;
+                        brickVisible = true;
+                        electroSpeed = false;
+                        electroSpeedEnhanced = false;
+                        gameState = Running;
+                        break;
+
+                    case GameWin:
+                        std::cout << "GameWin left click registered" << std::endl;
+                        lives = 3;
+                        ballVisible = true;
+                        brickVisible = true;
+                        electroSpeed = false;
+                        electroSpeedEnhanced = false;
+                        gameState = Running;
+                        break;
                     }
-                    Paddle.setFillColor(eleStorage[eleSwitcher]);
                 }
             }
             if (event.type == sf::Event::MouseButtonPressed)
             {
                 if (event.mouseButton.button == sf::Mouse::Right)
                 {
-                    std::cout << "Ball should be flying";
                     ballState = Flying;
                 }
             }
         }
-
-
-        switch (ballState)
+        switch (gameState)
         {
-            case Flying: 
-                ballPos.x += speed.x;
-                ballPos.y += speed.y;
-
-                if ((Ball.getPosition().x >= (screenSize.x - (ballSize * 2))) || (Ball.getPosition().x <= 0)) speed.x *= -1.0f;
-                if ((Ball.getPosition().y >= (screenSize.y - (ballSize * 2))) || (Ball.getPosition().y <= 0)) speed.y *= -1.0f;
+        case Running:
+            switch (ballState)
+            {
+            case Flying:
                 Ball.move(speed.x, speed.y);
+
+                if ((Ball.getPosition().x >= (screenSize.x - (ballSize * 2))) || (Ball.getPosition().x <= 0)) {
+                    speed.x *= -1.0f; // Reverse x speed on wall collision
+                }
+                if ((Ball.getPosition().y >= (screenSize.y - (ballSize * 2))) || (Ball.getPosition().y <= 0)) {
+                    speed.y *= -1.0f; // Reverse y speed on wall collision
+                }
+
                 break;
             case Reposition:
                 Ball.setPosition(mousePosition.x - ballSize, 730);
                 break;
-        }
-
-
-        // Update game logic
-        window.clear();
-        window.draw(Ball);
-        window.draw(Paddle);
-        if (brickVisible) {
-            window.draw(Brick);
-        }
-        window.display();
-
-        if (lives <= 0) 
-        {  
-            gameState = GameOver;
-        }
-
-        if (Ball.getPosition().y >= 770) {
-            lives -= 1;
-            std::cout << lives << std::endl;
-            ballState = Reposition;
-        }
-        mousePosition = sf::Mouse::getPosition(window);
-
-        if (mousePosition.x >= (screenSize.x - (paddleSize.x / 2))) mousePosition.x = (screenSize.x - (paddleSize.x / 2));
-        if (mousePosition.x <= (paddleSize.x / 2)) mousePosition.x = (paddleSize.x / 2);
-
-        Paddle.setPosition(mousePosition.x - (paddleSize.x / 2), 750);
-        Brick.setPosition(((screenSize.x / 2) - (brickSize.x / 2)), 200);
-        sf::FloatRect paddleBox = Paddle.getGlobalBounds();
-        sf::FloatRect ballBox = Ball.getGlobalBounds();
-        sf::FloatRect brickBox = Brick.getGlobalBounds();
-
-        if (electroSpeed) {
-            // Check how much time has passed since electroSpeed was activated
-            electroDuration = gameClock.getElapsedTime();
-
-            // Check if the duration has exceeded 3 seconds
-            if (electroDuration.asSeconds() >= 3) {
-                if (electroSpeedEnhanced == 1) {
-                    electroSpeed = false; // Reset the speed flag
-                    speed.x /= 2.0f;      // Reset speed to normal
-                    speed.y /= 2.0f;      // Reset speed to normal
-                }
-                else {
-                    electroSpeed = false; // Reset the speed flag
-                    speed.x /= 1.5f;      // Reset speed to normal
-                    speed.y /= 1.5f;
-                }
             }
-        }
 
-        if (paddleBox.intersects(ballBox))
-        {
-            speed.y *= -1.0f;
-            sf::Color elementChange = Paddle.getFillColor();
-            Ball.setFillColor(elementChange);
-        }
 
-        if (brickBox.intersects(ballBox) && brickVisible) {
-            brickVisible = false;
-            speed.y *= -1.0f;
-            if (Brick.getFillColor() == Electro)
-            { 
-                if (Ball.getFillColor() == Anemo) 
-                    {
-                        electroSpeedEnhanced = true;
-                        electroSpeed = true; // Activate electro speed
-                        speed.x *= 2.0f;     // Double the speed
-                        speed.y *= 2.0f;     // Double the speed
-                        gameClock.restart();  // Restart the game clock to track this effect duration
+            // Update game logic
+            window.clear();
+            window.draw(Ball);
+            window.draw(Paddle);
+            if (brickVisible) {
+                window.draw(Brick);
+            }
+            window.display();
+
+            if (lives <= 0)
+            {
+                gameState = GameOver;
+            }
+
+            if (Ball.getPosition().y >= 770) {
+                lives -= 1;
+                std::cout << lives << std::endl;
+                ballState = Reposition;
+            }
+            mousePosition = sf::Mouse::getPosition(window);
+
+            if (mousePosition.x >= (screenSize.x - (paddleSize.x / 2))) mousePosition.x = (screenSize.x - (paddleSize.x / 2));
+            if (mousePosition.x <= (paddleSize.x / 2)) mousePosition.x = (paddleSize.x / 2);
+
+            Paddle.setPosition(mousePosition.x - (paddleSize.x / 2), 750);
+            Brick.setPosition(((screenSize.x / 2) - (brickSize.x / 2)), 200);
+
+
+            if (electroSpeed) {
+                // Check how much time has passed since electroSpeed was activated
+                electroDuration = gameClock.getElapsedTime();
+
+                // Check if the duration has exceeded 3 seconds
+                if (electroDuration.asSeconds() >= 3) {
+                    if (electroSpeedEnhanced == 1) {
+                        electroSpeed = false; // Reset the speed flag
+                        speed.x /= 2.0f;      // Reset speed to normal
+                        speed.y /= 2.0f;      // Reset speed to normal
                     }
-                else 
-                {
-                    electroSpeedEnhanced = false;
-                    electroSpeed = true; // Activate electro speed
-                    speed.x *= 1.5f;     // Double the speed
-                    speed.y *= 1.5f;     // Double the speed
-                    gameClock.restart();
+                    else {
+                        electroSpeed = false; // Reset the speed flag
+                        speed.x /= 1.5f;      // Reset speed to normal
+                        speed.y /= 1.5f;
+                    }
                 }
             }
+
+            if (paddleBox.intersects(ballBox))
+            {
+                // Calculate the ball's position to ensure it bounces correctly
+                if (Ball.getPosition().y + ballSize * 2 >= Paddle.getPosition().y) {
+                    // Move the ball just above the paddle to avoid hovering
+                    Ball.setPosition(Ball.getPosition().x, Paddle.getPosition().y - ballSize * 2);
+                    speed.y *= -1.0f; // Reverse y-speed
+                    sf::Color elementChange = Paddle.getFillColor();
+                    Ball.setFillColor(elementChange);
+                }
+            }
+
+            if (brickBox.intersects(ballBox) && brickVisible) {
+                brickVisible = false;
+                speed.y *= -1.0f;
+                gameState = GameWin;
+                if (Brick.getFillColor() == Electro)
+                {
+                    if (electroSpeed == false) {
+                        if (Ball.getFillColor() == Anemo)
+                        {
+                            electroSpeedEnhanced = true;
+                            electroSpeed = true; // Activate electro speed
+                            speed.x *= 2.0f;     // Double the speed
+                            speed.y *= 2.0f;     // Double the speed
+                            gameClock.restart();  // Restart the game clock to track this effect duration
+                        }
+                        else
+                        {
+                            electroSpeedEnhanced = false;
+                            electroSpeed = true; // Activate electro speed
+                            speed.x *= 1.5f;     // Double the speed
+                            speed.y *= 1.5f;     // Double the speed
+                            gameClock.restart();
+                        }
+                    }
+                }
+            }
+            break;
+        case GameOver:
+            text.setFont(font);
+            text.setString("The abyss claims all.");
+            text.setCharacterSize(20);
+            text.setFillColor(Electro);
+            text.setPosition(screenSize.x / 2 - 100, screenSize.y / 2 - 25);
+
+            window.clear();
+            window.draw(text);
+            window.display();
+            break;
+
+        case GameWin:
+            text.setFont(font);
+            text.setString("Let the wind guide you!");
+            text.setCharacterSize(20);
+            text.setFillColor(Anemo);
+            text.setPosition(screenSize.x / 2 - 100, screenSize.y / 2 - 25);
+
+            window.clear();
+            window.draw(text);
+            window.display();
+            break;
+
         }
-
-
     }
 }

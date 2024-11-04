da7f8e10c196b837a174d56719332648978d9f0b
TheWanou21 <147034259+TheWanou21@users.noreply.github.com>
2024-11-04 11:55:39 +0100

    Maj

---
+++ Genshin Bricks/Decla_Game.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "Fct_Gen.h"
+#include "Decla_ball.h"
+#include "Decla_block.h"
+#include "Decla_Screen.h"
+#include "Decla_Paddle.h"
--- Genshin Bricks/Genshin Bricks.cpp
+++ Genshin Bricks/Genshin Bricks.cpp
@@ -1,8 +1,4 @@
-#include "Fct_Gen.h"
-#include "Decla_ball.h"
-#include "Decla_block.h"
-#include "Decla_Screen.h"
-#include "Decla_Paddle.h"
+#include "Decla_Game.h"
 
 int main() 
 {
